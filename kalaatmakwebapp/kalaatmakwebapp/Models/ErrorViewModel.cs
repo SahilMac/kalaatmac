d7f89adf80daaf8ced1df9a94b4b18df2e9ef0d6
Sahil Macwan <sahilmacwan5@gmail.com>
2023-03-09 09:45:58 +0530

    added dotnet webapp

---
+++ kalaatmakwebapp/kalaatmakwebapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace kalaatmakwebapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
